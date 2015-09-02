be17cc2d66c49a208067c9406409ceca60471676
bugoodby <bugoodby@gmail.com>
2015-09-02 21:11:32 +0900

    update

---
+++ script/wsh/tips/24.extract_file/ExtractFromFile.js
@@ -0,0 +1,54 @@
+//------------------------------------------------
+// D&Dされたヘッダファイルから
+//  - cpca_attribute.csv
+//  - cpca_attdef.jsの[global tables]以降
+// を更新
+//
+//------------------------------------------------
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var wshShell = new ActiveXObject("WScript.Shell");
+
+WScript.Quit(main( WScript.Arguments.Count(), WScript.Arguments ));
+
+function main( argc, argv ) 
+{
+	if ( argc < 1 ) {
+		WScript.Echo("ファイルを指定してください");
+		return 1;
+	}
+	// WScriptならCScriptで起動し直し
+	if ( WScript.FullName.substr(WScript.FullName.length - 11, 11).toUpperCase() == "WSCRIPT.EXE" ) {
+		//var wshShell = new ActiveXObject("WScript.Shell");
+		var strCmd = "cmd.exe /c cscript.exe //NOLOGO \"" + WScript.ScriptFullName + "\"";
+		for ( var i = 0; i < argc; i++ ) {
+			strCmd += " \"" + argv(i) + "\"";
+		}
+		strCmd += " | more & pause"
+		return wshShell.Run(strCmd, 1, true);
+	}
+	
+	ExtractFromFile(argv(0));
+	
+	return 0;
+}
+
+// ファイルを１行ずつ舐め、正規表現に一致したら別ファイルに出力
+function ExtractFromFile( filespec )
+{
+	var out_file = WScript.ScriptFullName.replace(WScript.ScriptName, "ExtractFromFile.txt");
+	
+	var ws = fso.OpenTextFile(out_file, 2/*ForWriting*/, true, 0);
+	var rs = fso.OpenTextFile(filespec, 1/*ForReading*/, false, 0);
+	var text = rs.ReadAll();
+	rs.Close();
+	var lines = text.split(/\r\n/);
+	
+	for ( var i in lines ) {
+		if ( lines[i].match(/^#define\s+([a-zA-Z0-9_]+)\s+([0-9]+).*/) ) {
+			WScript.Echo(lines[i]);
+			ws.WriteLine(RegExp.$2 + "," + RegExp.$1);
+		}
+	}
+	
+	ws.Close();
+}
